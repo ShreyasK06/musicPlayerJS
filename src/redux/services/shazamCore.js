2663ef5e3fc4402e30b86fb1d0d831c33c935f33
ShreyasK06 <shreyaskakkar@gmail.com>
2023-07-10 00:23:49 -0400

    summary

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
+
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://shazam-core7.p.rapidapi.com',
@@ -11,10 +12,14 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in_world_by_genre?genre=POP'}),
+        getTopCharts: builder.query({ query: () => '/charts/get-top-songs-in_country_by_genre?genre=POP&country_code=US' }),
+        getTopPlays: builder.query({query: () =>'/charts/get-top-songs-in_world_by_genre?genre=POP' }),
+        getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`}),
     }),
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetTopPlaysQuery,
+    useGetSongDetailsQuery,
 } = shazamCoreApi;
